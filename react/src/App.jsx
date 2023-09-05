5a1744f42c1d1aa7ae9286fdd6b0002351689e43
Favour Ukonu <favouroukonu@gmail.com>
2023-09-05 03:47:49 +0100

    created-counter-app

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import * as Icon from "react-feather";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // increase by 1
+  const increaseByOne = (e) => {
+    e.preventDefault();
+    setCount((count) => count + 1);
+  };
+
+  // increase by 10
+  const increaseByTen = (e) => {
+    e.preventDefault();
+    setCount((count) => count + 10);
+  };
+
+  // reset to zero(0)
+  const reset = (e) => {
+    e.preventDefault();
+    setCount(0);
+  };
+
+  // generate random number between 1 and 100
+  const randomNumber = (e) => {
+    e.preventDefault();
+    setCount(Math.floor(Math.random() * 100));
+  };
+
+  //decrease by 10
+  const decreaseByTen = (e) => {
+    e.preventDefault();
+    setCount((count) => count - 10);
+  };
+
+  // decrease by 1
+  const decreaseByOne = (e) => {
+    e.preventDefault();
+    setCount((count) => count - 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section>
+      <section className="count-section">
+        <h5>Current Value:</h5>
+
+        <h1 className="count-num">{count}</h1>
+
+        <div className="count-buttons">
+          <button onClick={increaseByOne}>
+            <Icon.ChevronUp />
+          </button>
+          <button onClick={increaseByTen}>
+            <Icon.ChevronsUp />
+          </button>
+          <button onClick={reset}>
+            <Icon.RotateCcw />
+          </button>
+          <button onClick={randomNumber}>
+            <Icon.Hash />
+          </button>
+          <button onClick={decreaseByTen}>
+            <Icon.ChevronsDown />
+          </button>
+          <button onClick={decreaseByOne}>
+            <Icon.ChevronDown />
+          </button>
+        </div>
+      </section>
+    </section>
+  );
 }
 
-export default App
+export default App;
